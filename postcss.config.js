339d5f2c37393615f77d86f150a2a439a65e2c9c
alexsincai <alex.sincai@yahoo.co.uk>
2022-11-16 11:51:09 +0200

    better

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    plugins: [require("autoprefixer")],
+    plugins: [
+        require("autoprefixer"),
+        require("postcss-preset-env")({
+            stage: 1,
+        }),
+        require("postcss-import"),
+        require("postcss-assets")({
+            loadPaths: ["dist/img"],
+        }),
+        require("cssnano")({
+            preset: "default",
+        }),
+    ],
 };
